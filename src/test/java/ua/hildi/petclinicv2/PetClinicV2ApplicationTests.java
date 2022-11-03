1a92053982c5c32de5800364a9dd23f61d4b162b
serhiihildi <sergeygildi@gmail.com>
2022-11-03 16:57:35 +0200

    feat: initializing, fill pom.xml

---
+++ src/test/java/ua/hildi/petclinicv2/PetClinicV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.hildi.petclinicv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
